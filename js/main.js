66278673859485dca28827c4f8fa07d09b47a776
opazisimo <girasolesporlavida@gmail.com>
2016-10-14 19:57:31 -0300

    Arreglo de navbar, se quita slide y se agrega banner

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
+// Navbar
 jQuery('ul.nav li.dropdown').hover(function() {
   jQuery(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn();
 }, function() {
   jQuery(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut();
+});
+
+// Banner
+
+$(function() {
+var ban = $('#banner');
+var backgrounds = new Array(
+'url(../bongazo/img/bg1.png)',
+'url(../bongazo/img/bg2.png)',
+'url(../bongazo/img/bg3.png)'
+);
+var current = 0;
+
+function nextBackground() {
+ban.css(
+'background',
+backgrounds[current = ++current % backgrounds.length]
+);
+
+setTimeout(nextBackground, 2000);
+}
+setTimeout(nextBackground, 2000);
+ban.css('background', backgrounds[0]);
 });
